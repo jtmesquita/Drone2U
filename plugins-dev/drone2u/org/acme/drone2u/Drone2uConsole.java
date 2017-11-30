819bf33a971041aaa0dc9c5e78a30f8042a2bbc9
pedro <pedro@pedro-N552VX>
2017-11-30 21:40:05 +0000

    Tentativa incial da criação de multiplos pointos. Penso que seja por este caminho..

--- plugins-dev/drone2u/org/acme/drone2u/Drone2uConsole.java
+++ plugins-dev/drone2u/org/acme/drone2u/Drone2uConsole.java
@@ -34,6 +34,9 @@ package org.acme.drone2u;
 
 import java.awt.event.ActionEvent;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Vector;
+
 import javax.swing.AbstractAction;
 import javax.swing.Action;
 import javax.swing.JButton;
@@ -45,6 +48,7 @@ import pt.lsts.imc.PlanControl;
 import pt.lsts.imc.PlanDB;
 import pt.lsts.imc.PlanSpecification;
 import pt.lsts.imc.SetEntityParameters;
+import pt.lsts.neptus.NeptusLog;
 import pt.lsts.neptus.comm.IMCSendMessageUtils;
 import pt.lsts.neptus.console.ConsoleLayout;
 import pt.lsts.neptus.console.ConsolePanel;
@@ -53,6 +57,7 @@ import pt.lsts.neptus.console.events.ConsoleEventNewSystem;
 import pt.lsts.neptus.console.events.ConsoleEventPlanChange;
 import pt.lsts.neptus.console.plugins.PlanChangeListener;
 import pt.lsts.neptus.i18n.I18n;
+import pt.lsts.neptus.mp.Maneuver;
 import pt.lsts.neptus.mp.ManeuverLocation;
 import pt.lsts.neptus.mp.MissionChangeEvent;
 import pt.lsts.neptus.mp.maneuvers.Goto;
@@ -61,7 +66,9 @@ import pt.lsts.neptus.plugins.Popup;
 import pt.lsts.neptus.plugins.Popup.POSITION;
 import pt.lsts.neptus.types.coord.LocationType;
 import pt.lsts.neptus.types.mission.MissionType;
+import pt.lsts.neptus.types.mission.TransitionType;
 import pt.lsts.neptus.types.mission.plan.PlanType;
+import pt.lsts.neptus.util.GuiUtils;
 
 /**
  * @author joao
@@ -73,6 +80,8 @@ import pt.lsts.neptus.types.mission.plan.PlanType;
 @Popup(pos = POSITION.CENTER, width = 200, height = 200, accelerator = 'Y')
 @SuppressWarnings("serial")
 public class Drone2uConsole extends ConsolePanel{
+    
+    private final String defaultCondition = "ManeuverIsDone";
 
     /**
      * @param console
@@ -91,46 +100,112 @@ public class Drone2uConsole extends ConsolePanel{
         return cl.getImcMsgManager().sendMessageToVehicle(pspec, vehicleID, null);
     }
     
+    private String getNewManeuverName(PlanType plan, String manType) {
+
+        int i = 1;
+        while (plan.getGraph().getManeuver(manType + i) != null)
+            i++;
+
+        return manType + i;
+    }    
    
 
     
     
     public PlanControl buildPlan (String vehicleID, MissionType mt, LocationType [] destArray, double speed, double height) {   
-        
+
         PlanType neptusPlan = new PlanType(mt);
         neptusPlan.addVehicle(vehicleID); 
-        
+
+        int aux = 0;
+
         for(LocationType dest : destArray) {      //para futura implementação de multiplos pontos (para já destArray tem apenas uma posiçao)    
-            dest.convertToAbsoluteLatLonDepth();           
-            
-            ManeuverLocation maneuverLoc = new ManeuverLocation(dest);
-            maneuverLoc.setZ(height);
-            maneuverLoc.setZUnits(ManeuverLocation.Z_UNITS.HEIGHT); 
-            
-            Goto point = new Goto();
-            point.setManeuverLocation(maneuverLoc);           
-            point.setSpeed(speed);     
-            
-            neptusPlan.getGraph().addManeuver(point);     
+
+            if (aux == 0) {             //se for a primeira manobra adiciona a primeira manobra
+                dest.convertToAbsoluteLatLonDepth();           
+
+                ManeuverLocation maneuverLoc = new ManeuverLocation(dest);
+                maneuverLoc.setZ(height);
+                maneuverLoc.setZUnits(ManeuverLocation.Z_UNITS.HEIGHT); 
+
+                Goto point = new Goto();
+                point.setManeuverLocation(maneuverLoc);           
+                point.setSpeed(speed);     
+
+                neptusPlan.getGraph().addManeuver(point);
                 
+                aux++;
+            }
             //neptusPlan.getGraph().addManeuverAtEnd(point);                
             //neptusPlan.getGraph().addTransition(smid, maneuverLoc.getId(), true);
-            System.out.println(neptusPlan.getGraph().getExitingTransitions(neptusPlan.getGraph().getManeuver(maneuverLoc.getId())));            
+            //System.out.println(neptusPlan.getGraph().getExitingTransitions(neptusPlan.getGraph().getManeuver(maneuverLoc.getId())));
+
+            else {
+
+                ManeuverLocation maneuverLoc = new ManeuverLocation(dest);
+                maneuverLoc.setZ(height);
+                maneuverLoc.setZUnits(ManeuverLocation.Z_UNITS.HEIGHT);
+
+                //Maneuver nextMan = neptusPlan.getGraph().getFollowingManeuver(manID);
+                Maneuver lastMan = neptusPlan.getGraph().getLastManeuver();
+
+                Goto point = new Goto();
+
+                try {
+                    point.setProperties(lastMan.getProperties());     // coloco no novo ponto as propriedades do anterior
+                }
+                catch (Exception e) {
+                    NeptusLog.pub().error(e, e);
+                }
+                point.cloneActions(lastMan);
+
+                point.setId(getNewManeuverName(neptusPlan, "GOTO"));
+
+                neptusPlan.getGraph().addManeuver(point);
+
+                Vector<TransitionType> addedTransitions = new Vector<TransitionType>();
+                Vector<TransitionType> removedTransitions = new Vector<TransitionType>();
+
+                /*verifico se o novo ponto criado não é nulo
+                if (point == null) {
+                    GuiUtils.errorMessage(this, I18n.text("Error adding maneuver"),
+                            I18n.textf("The maneuver %maneuverType can't be added", point.getType()));
+                    return null;
+                }*/
+
+                /* verifico se a ultima manobra não é nula e se o novo ponto
+                   não é a manobra que já foi adicionada
+                   se não for adiciono as transições*/
+                if (lastMan != null && lastMan != point) {
+
+                    if (neptusPlan.getGraph().getExitingTransitions(lastMan).size() != 0) {
+                        for (TransitionType exitingTrans : neptusPlan.getGraph().getExitingTransitions(lastMan)) {
+                            removedTransitions.add(neptusPlan.getGraph().removeTransition(exitingTrans.getSourceManeuver(),
+                                    exitingTrans.getTargetManeuver()));
+                        }
+                    }
+
+                    addedTransitions.add(neptusPlan.getGraph().addTransition(lastMan.getId(), point.getId(), defaultCondition));
+                }
                 
+                neptusPlan.getGraph().addManeuver(point);
+
+            }
+
         }
-        
-        
-        
+
+
+
         PlanControl pc = new PlanControl(); 
         pc.setType(PlanControl.TYPE.REQUEST);
         pc.setOp(PlanControl.OP.START);
         pc.setRequestId(IMCSendMessageUtils.getNextRequestId());    // IMCSendMessageUtils.... é apenas para fins de sincronização   
         pc.setPlanId(neptusPlan.getId());
         System.out.println(neptusPlan.asIMCPlan().asXml(true));
-       
-        
+
+
         PlanSpecification pSpec = new PlanSpecification(neptusPlan.asIMCPlan());    // criação da mensagem IMC para ser mandada para p drone
-        
+
         SetEntityParameters heightc = new SetEntityParameters();
         heightc.setName("Height Control");
         EntityParameter activeP = new EntityParameter("Active", "true");
@@ -147,24 +222,24 @@ public class Drone2uConsole extends ConsolePanel{
         pathController.setParams(Arrays.asList(new EntityParameter[]{
                 new EntityParameter("Use controller", "true")
         }));        
-        
-        
+
+
         // alteração da configuração do drone de modo a desligar a opção Ardupilot
         // e ativação das opções Height control e PathControl
         try {
-        pSpec.setStartActions(Arrays.asList(new SetEntityParameters[] {
-                heightc,
-                autopilot,
-                pathController
-        }));
+            pSpec.setStartActions(Arrays.asList(new SetEntityParameters[] {
+                    heightc,
+                    autopilot,
+                    pathController
+            }));
         }
-        
+
         catch (Exception e1) {                    
             e1.printStackTrace();
         }  
-        
+
         pc.setArg(pSpec);     // envio do plano para o drone
-        
+
         return pc; 
     } 
 
@@ -176,15 +251,15 @@ public class Drone2uConsole extends ConsolePanel{
         Action newPointAction = new AbstractAction(I18n.text("New Goto point...")) {
             @Override
             public void actionPerformed(ActionEvent e) {                
-                LocationType[] destArray = new LocationType[1];
+                LocationType[] destArray = new LocationType[2];
                 
                 destArray[0] = new LocationType();
                 destArray[0].setLongitudeStr("6W42'31.15''");
                 destArray[0].setLatitudeStr("41N51'27.24''");
                 
-                /*destArray[1] = new LocationType();
+                destArray[1] = new LocationType();
                 destArray[1].setLongitudeStr("6W43'31.15''");
-                destArray[1].setLatitudeStr("41N51'27.24''");*/             
+                destArray[1].setLatitudeStr("41N51'27.24''");             
                 
                 
                 PlanControl pc = buildPlan("x8-02", getConsole().getMission(),
